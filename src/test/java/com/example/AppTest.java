7756dda03108d7831c4afaf3c349c62d5483d68a
Vasanth-V1 <vasanth.v2023@vitstudent.ac.in>
2026-02-19 16:37:18 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,3 +1,10 @@
-public class App {
-    
+package com.example;
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class AppTest {
+ @Test
+ public void testAdd() {
+ App app = new App();
+ assertEquals(5, app.add(2, 3));
+ }
 }
